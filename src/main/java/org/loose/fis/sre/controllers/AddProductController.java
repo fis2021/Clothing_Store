01d9a16b86b66bb97b199dcfe6979ee24da0c3c7
denisaSuciu <deniisaoanaa@gmail.com>
2021-05-13 22:06:56 +0300

    Add product image button + My Products page

--- src/main/java/org/loose/fis/sre/controllers/AddProductController.java
+++ src/main/java/org/loose/fis/sre/controllers/AddProductController.java
@@ -1,4 +1,46 @@
 package org.loose.fis.sre.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import  javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class AddProductController {
-}
+    @FXML
+    ImageView photoPath;
+    @FXML
+    public Button finishButton;
+    public File file;
+    public String path;
+    public void addPictureOnAction() throws MalformedURLException{
+        Stage stage=new Stage();
+        FileChooser chooser=new FileChooser();
+        chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("jpg files", "*.jpg"));
+        file= chooser.showOpenDialog(stage);
+        path=file.getAbsolutePath();
+        chooser.setInitialDirectory(file.getParentFile());
+        File file=new File(path);
+        String url=file.toURI().toURL().toExternalForm();
+        Image pic=new Image(url,false);
+        photoPath.setImage(pic);
+        photoPath.setFitHeight(285);
+        photoPath.setFitWidth(285);
+        photoPath.rotateProperty();
+    }
+    public void setFinishButton(ActionEvent event) throws IOException {
+        Stage stageBack = (Stage) finishButton.getScene().getWindow();
+        stageBack.setTitle("My Products");
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MyProducts.fxml"));
+        stageBack.setScene(new Scene(root, 600, 350));
+        stageBack.show();
+    }
+
+    }
